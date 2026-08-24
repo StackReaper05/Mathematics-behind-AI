79785e7962a2487fc2c37fd0beae897f78d784fb
Maahir <124433813+StackReaper05@users.noreply.github.com>
2026-08-24 22:44:05 +0530

    Add files via upload

---
+++ Assignment-3/Q1.py
@@ -0,0 +1,23 @@
+import sympy as sp
+x,y=sp.symbols('x y')
+f=x**2*y + 3*x*y**2
+
+#1
+df_dx=sp.diff(f,x)
+df_dy=sp.diff(f,y)
+print("\nFunction: ",f)
+print(f"\ndf/dx = {df_dx}")
+print(f"df/dy = {df_dy}")
+
+#2
+gradient=sp.Matrix([df_dx,df_dy])
+print("\nGradient Vector: ",gradient)
+
+#3
+point={x:2,y:1}
+gradient_at_point=gradient.subs(point)
+print(f"\nGradient at ({point[x]},{point[y]}) = {gradient_at_point}")
+
+#4
+magnitude=sp.sqrt(gradient_at_point[0]**2 + gradient_at_point[1]**2)
+print("\nMagnitude of gradient: ",magnitude, magnitude.evalf())
